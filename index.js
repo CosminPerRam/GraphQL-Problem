a8e9d3fe734855c46bc1f28934b1b13c2d5f5840
CosminPerRam <cosmin.p@live.com>
2022-05-19 21:46:51 +0300

    Partial working part 2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { graphql, buildSchema } = require('graphql');
+const {GraphQLObjectType, GraphQLList, GraphQLInt, GraphQLID, GraphQLInputObjectType, GraphQLString, GraphQLSchema, GraphQLNonNull} = require('graphql');
 const { graphqlHTTP } = require('express-graphql');
 const { Sequelize } = require('sequelize');
 const credentials = require('./credentials.json');
+const { resolver } = require('graphql-sequelize');
 
 var sequelize = new Sequelize(credentials.db_name, credentials.username, credentials.password, {
     host: credentials.host,
@@ -18,64 +20,124 @@ sequelize.authenticate()
     process.exit(1);
   });
 
-const schema = buildSchema(`
-    type Query {
-        persons: [Person]
-        jobs: [Job]
-    }
+let Person = sequelize.define('person', {
+    name: Sequelize.STRING
+});
 
-    type Person {
-        name: String
-    }
+let Employee = sequelize.define('employee', {
+    name: Sequelize.STRING
+});
 
-    type Job {
-        description: String
-        employee: Employee
-        employer: String
-    }
+let Job = sequelize.define('job', {
+    description: Sequelize.STRING,
+    employer: Sequelize.STRING
+});
 
-    type Employee {
-        name: String
-    }
-`);
+Job.Employee = Job.hasOne(Employee, {as: 'employee'});
 
-const allPersons = [
-    {
-        name: "someone"
-    },
-    {
-        name: "anotherone"
-    }
-]
-const allJobs = [
-    {
-        description: "Banana collector",
-        employee: {
-            name: "Cosmin"
+let personType = new GraphQLObjectType({
+    name: `Person`,
+    description: 'A Person',
+    fields: {
+        id: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: 'The id of the person.'
         },
-        employer: "Bezos"
-    },
-    {
-        description: "Discord admin",
-        employee: {
-            name: "Lucian"
-        },
-        employer: "Reddit moderator"
+        name: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: 'The name of the person.'
+        }
     }
-]
+});
 
-const graphRoot = {
-    persons: () => (allPersons),
-    jobs: () => (allJobs)
-};
+let employeeType = new GraphQLObjectType({
+    name: `Employee`,
+    description: 'An Employee',
+    fields: {
+        id: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: 'The id of the employee.'
+        },
+        name: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: 'The name of the employee.'
+        }
+    }
+});
+
+let employeeInput = new GraphQLInputObjectType({
+    name: `EmployeeInput`,
+    fields: {
+        name: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: 'The name of the employee.'
+        }
+    }
+});
+
+let jobType = new GraphQLObjectType({
+    name: `Job`,
+    description: 'A Job',
+    fields: {
+        id: {
+            type: new GraphQLNonNull(GraphQLInt),
+            description: 'The id of the job.'
+        },
+        employer: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: 'The name of the employer.'
+        },
+        description: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: 'The description.'
+        },
+        employee: {
+            type: new GraphQLNonNull(employeeType),
+            resolve: resolver(Job.Employee)
+        }
+    }
+});
+
+let schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'root',
+      fields: {
+        jobs: {
+            type: new GraphQLList(jobType),
+            resolve: resolver(Job)
+        },
+        persons: {
+            type: new GraphQLList(personType),
+            resolve: resolver(Person)
+        }
+      }
+    }),
+    mutation: new GraphQLObjectType({
+        name: 'createJob',
+        fields: {
+            createJob: {
+                type: jobType,
+                args: {
+                    employer: { type: new GraphQLNonNull(GraphQLString) },
+                    description: { type: new GraphQLNonNull(GraphQLString) },
+                    employee: { type: new GraphQLNonNull(employeeInput) }
+                },
+                resolve: (root, job, info) => {
+                    console.log(job.employee);
+                    Employee.create(job.employee);
+                    return Job.create(job);
+                }
+            }
+        }
+    })
+  });
 
 const app = express();
 
-app.use(
-  '/graphql',
+app.use('/graphql',
   graphqlHTTP({
     schema,
-    rootValue: graphRoot,
+    rootValue: resolver,
     graphiql: true,
   }),
 );
